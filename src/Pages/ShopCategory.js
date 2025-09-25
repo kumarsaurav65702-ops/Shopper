193ce48d2b2270315c31542e2fba8f3c2c4e3432
Saurav <kumarsaurav65701@gmail.com>
2025-09-25 10:47:16 +0530

    added login page

--- src/Pages/ShopCategory.js
+++ src/Pages/ShopCategory.js
@@ -1,28 +1,38 @@
 import React, { useContext } from 'react';
 import './CSS/ShopCategory.css';
 import { ShopContext } from '../Context/ShopContext';
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import Item from '../Components/Item/Item'
 
 const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
 
-  // Filter products by category
-  const filteredProducts = all_product.filter(
-    (item) => item.category === props.category
-  );
+
 
   return (
     <div className='shop-category'>
-      <img src={props.banner} alt="" className="category-banner" />
-
-      <div className="products">
-        {filteredProducts.map((product) => (
-          <div key={product.id} className="product-card">
-            <img src={product.image} alt={product.name} />
-            <h3>{product.name}</h3>
-            <p>₹{product.price}</p>
-          </div>
-        ))}
+      <img src={props.banner} alt="" className="shopcategory-banner" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
       </div>
+      <div className="shopcategory-products">
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+          }
+          else {
+            return null;
+          }
+        })}
+      </div>
+<div className="shopcategory-loadmore">
+  Explore More
+</div>
     </div>
   );
 };
